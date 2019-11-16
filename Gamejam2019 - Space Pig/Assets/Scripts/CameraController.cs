167165cda7b9b4ef6da545c73a7b8d3005316a4d
Thomas Steenfeldt Laursen <tlaur18@student.sdu.dk>
2019-11-16 17:11:17 +0100

    Camera never goes back

---
+++ Gamejam2019 - Space Pig/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;          //Public variable to store a reference to the player game object
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    private Vector3 playerPositionBefore;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+        playerPositionBefore = player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(playerPositionBefore.x, player.transform.position.y, player.transform.position.z) + offset;
+
+        if (player.transform.position.x > playerPositionBefore.x)
+        {
+            // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+            transform.position = player.transform.position + offset;
+            playerPositionBefore = player.transform.position;
+        }
+    }
+}
